da9f60e2668c49d6de0209c12bd8b4a4d981f602
c4rlos316 <cama619@gmail.com>
2026-07-01 19:58:06 -0600

    feat: Creamos la parte del backend para el blog personal

---
+++ backend/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
